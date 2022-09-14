eec4a1db0fffe9903d56887d8efaf9b6213c81f6
Siddharth Singh Rathour <rathour_siddharth_singh@lilly.com>
2022-09-14 12:59:30 +0530

    Function_Class Commit

---
+++ function.py
@@ -0,0 +1,9 @@
+def function1():
+    print(f"This is my First Function")
+    return
+function1()
+
+def function2(name,id):
+    print(f"This is my:{name} and my id is:{id}")
+
+function2("siddharth","121")
